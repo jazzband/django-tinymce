e036aecd3c0d078740b3b73b5e8e5ea9d3fd016c
joost@cassee.net <joost@cassee.net@d77dbfcb-f654-0410-9b52-53af79b86e66>
2008-08-27 13:44:10 +0000

    Enabled content language specific Javascript view.

--- tinymce/urls.py
+++ tinymce/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tinymce.views',
     url(r'^js/textareas/(?P<name>.+)/$', 'textareas_js', name='tinymce-js'),
-    #url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', 'textareas_js', name='tinymce-js'),
+    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', 'textareas_js', name='tinymce-js-lang'),
     url(r'^spellchecker/$', 'spell_check'),
     url(r'^flatpages_link_list/$', 'flatpages_link_list'),
 )
