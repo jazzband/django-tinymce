52871f4afc16f0de0eb3313fa4fb2488eb9baa44
joost@cassee.net <joost@cassee.net@d77dbfcb-f654-0410-9b52-53af79b86e66>
2010-01-08 23:32:18 +0000

    Fixed import of smart_unicode.

--- tinymce/widgets.py
+++ tinymce/widgets.py
@@ -11,7 +11,10 @@ from django.conf import settings
 from django.contrib.admin import widgets as admin_widgets
 from django.core.urlresolvers import reverse
 from django.forms.widgets import flatatt
-from django.forms.util import smart_unicode
+try:
+    from django.utils.encoding import smart_unicode
+except ImportError:
+    from django.forms.util import smart_unicode
 from django.utils.html import escape
 from django.utils import simplejson
 from django.utils.datastructures import SortedDict
