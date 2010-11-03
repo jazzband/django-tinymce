46cf978b634e72a433a153c933edd6c710503588
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2010-11-03 01:41:44 +0100

    changed settings.STATIC_URL to getattr(settings, 'STATIC_URL', '') as requested in issue #91 http://code.google.com/p/django-tinymce/issues/detail?id=91

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -12,8 +12,8 @@ USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER',
         'filebrowser' in settings.INSTALLED_APPS)
 
 if 'staticfiles' in settings.INSTALLED_APPS:
-    JS_URL = os.path.join(settings.STATIC_URL, 'tiny_mce/tiny_mce.js')
-    JS_ROOT = os.path.join(settings.STATIC_ROOT, 'tiny_mce')
+    JS_URL = os.path.join(getattr(settings, 'STATIC_URL', ''), 'tiny_mce/tiny_mce.js')
+    JS_ROOT = os.path.join(getattr(settings, 'STATIC_ROOT', ''), 'tiny_mce')
 else:
     JS_URL = getattr(settings, 'TINYMCE_JS_URL',
             '%sjs/tiny_mce/tiny_mce.js' % settings.MEDIA_URL)
