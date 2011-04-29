ff88e1242ad8a6e384c21d180a8d0e7d4dfa3831
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2011-04-29 02:49:53 +0200

    added check for django.contrib.staticfiles

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -11,7 +11,7 @@ USE_COMPRESSOR = getattr(settings, 'TINYMCE_COMPRESSOR', False)
 USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER',
         'filebrowser' in settings.INSTALLED_APPS)
 
-if 'staticfiles' in settings.INSTALLED_APPS:
+if 'staticfiles' in settings.INSTALLED_APPS or 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
     JS_URL = os.path.join(getattr(settings, 'STATIC_URL', ''), 'tiny_mce/tiny_mce.js')
     JS_ROOT = os.path.join(getattr(settings, 'STATIC_ROOT', ''), 'tiny_mce')
 else:
