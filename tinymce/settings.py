76b3b604cd604d4964a2748ee7940d99bc9134c9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-10-26 13:22:32 +0200

    Allow overriding the JS_URL and JS_ROOT settings.

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -11,11 +11,19 @@ USE_COMPRESSOR = getattr(settings, 'TINYMCE_COMPRESSOR', False)
 USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER',
         'filebrowser' in settings.INSTALLED_APPS)
 
+JS_URL = getattr(settings, 'TINYMCE_JS_URL', None)
+JS_ROOT = getattr(settings, 'TINYMCE_JS_ROOT', None)
+
+# Allways allow overriding of the default settings
 if 'staticfiles' in settings.INSTALLED_APPS or 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
-    JS_URL = getattr(settings, 'TINYMCE_JS_URL',os.path.join(settings.STATIC_URL, 'tiny_mce/tiny_mce.js'))
-    JS_ROOT = getattr(settings, 'TINYMCE_JS_ROOT',os.path.join(settings.STATIC_ROOT, 'tiny_mce'))
+    if not JS_URL:
+        JS_URL = os.path.join(getattr(settings, 'STATIC_URL', ''), 'tiny_mce/tiny_mce.js')
+    if not JS_ROOT:
+        JS_ROOT = os.path.join(getattr(settings, 'STATIC_ROOT', ''), 'tiny_mce')
 else:
-    JS_URL = getattr(settings, 'TINYMCE_JS_URL','%sjs/tiny_mce/tiny_mce.js' % settings.MEDIA_URL)
-    JS_ROOT = getattr(settings, 'TINYMCE_JS_ROOT', os.path.join(settings.MEDIA_ROOT, 'js/tiny_mce'))
+    if not JS_URL:
+        JS_URL = '%sjs/tiny_mce/tiny_mce.js' % settings.MEDIA_URL
+    if not JS_ROOT:
+        JS_ROOT = os.path.join(settings.MEDIA_ROOT, 'js/tiny_mce')
 
 JS_BASE_URL = JS_URL[:JS_URL.rfind('/')]
