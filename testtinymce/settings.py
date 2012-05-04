f3d4b923f9b498208455ce812e8f759accc5a299
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2012-05-04 02:37:04 +0200

    added flatpages to testtinymce

--- testtinymce/settings.py
+++ testtinymce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.flatpages',
     'tinymce',
     'testapp',
 )
