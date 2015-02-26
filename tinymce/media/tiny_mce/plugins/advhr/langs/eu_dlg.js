facca7118663125f725cf0cc12a1fb38165b96f8
Steffen Jasper <jesus_at_hollywoodparty@gmx.net>
2015-02-26 13:01:40 +0100

    TinyMCE 3.5.11
    Updated TinyMCE to 3.5.11 and applied language files downloaded at
    2015/02/26

--- tinymce/media/tiny_mce/plugins/advhr/langs/eu_dlg.js
+++ tinymce/media/tiny_mce/plugins/advhr/langs/eu_dlg.js
@@ -1,1 +1,1 @@
-tinyMCE.addI18n('eu.advhr_dlg',{size:"Altuera",noshade:"Itzalik gabe",width:"Zabalera",normal:"Normal",widthunits:"Units"});
+tinyMCE.addI18n('eu.advhr_dlg',{size:"Altuera",noshade:"Itzalik gabe",width:"Zabalera",normal:"Ohikoa",widthunits:"Unitateak"});
