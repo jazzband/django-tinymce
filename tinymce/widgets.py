27e84a36eeef2d760b3edf56a7de9f5f7ba85c38
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-07-23 16:34:21 +0200

    Fixes #114 — SortedDict doesn't exist anymore.

--- tinymce/widgets.py
+++ tinymce/widgets.py
@@ -14,7 +14,10 @@ from django.contrib.admin import widgets as admin_widgets
 from django.core.urlresolvers import reverse
 from django.forms.widgets import flatatt
 from django.utils.html import escape
-from django.utils.datastructures import SortedDict
+try:
+    from django.utils.datastructures import SortedDict
+except ImportError:
+    from collections import OrderedDict as SortedDict
 from django.utils.safestring import mark_safe
 from django.utils.translation import get_language, ugettext as _
 import json
@@ -129,7 +132,8 @@ def get_language_config(content_language=None):
 
     lang_names = SortedDict()
     for lang, name in settings.LANGUAGES:
-        if lang[:2] not in lang_names: lang_names[lang[:2]] = []
+        if lang[:2] not in lang_names:
+            lang_names[lang[:2]] = []
         lang_names[lang[:2]].append(_(name))
     sp_langs = []
     for lang, names in lang_names.items():
