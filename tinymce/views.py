f094eeac98db5f9f34a91852d432cc2bec9fcec1
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-07-23 16:28:49 +0200

    Fixes #69 — Use request.body rather than request.raw_post_data.

--- tinymce/views.py
+++ tinymce/views.py
@@ -42,7 +42,7 @@ def spell_check(request):
     try:
         import enchant
 
-        raw = request.raw_post_data
+        raw = request.body
         input = json.loads(raw)
         id = input['id']
         method = input['method']
