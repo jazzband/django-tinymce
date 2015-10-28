6897eea75e4c6fc493794e8e96ed870563eb1340
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2015-10-28 23:19:27 +0200

    Fix tinymce statics path to js library

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -1,7 +1,7 @@
 import os
 import warnings
 from django.conf import settings
-from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.staticfiles import finders
 
 OLD_CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', False)
 if OLD_CONFIG:
@@ -10,8 +10,10 @@ DEFAULT_CONFIG = OLD_CONFIG or {'theme': "modern", 'relative_urls': False}
 
 if getattr(settings, 'TINYMCE_JS_URL', False):
     raise RuntimeError("TINYMCE_JS_URL is not supported anymore, check docs for instructions.")
-JS_URL = staticfiles_storage.url('tinymce/tinymce.min.js')
-JS_ROOT = staticfiles_storage.url('tinymce')
+
+JS_URL = getattr(settings, 'TINYMCE_JS_URL', os.path.join(settings.STATIC_URL, 'tinymce/tinymce.min.js'))
+JS_ROOT = getattr(settings, 'TINYMCE_JS_ROOT', finders.find('tinymce', all=False))
+
 JS_BASE_URL = JS_URL[:JS_URL.rfind('/')]
 
 if getattr(settings, 'TINYMCE_SPELLCHECKER', False):
