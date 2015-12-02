129f88e41ee604c9e304e519008f4939ba34b2b9
Aarni Koskela <akx@iki.fi>
2015-12-02 19:29:37 +0200

    Pass views as callables, not as strings
    Conflicts:
    	tinymce/urls.py

--- tinymce/urls.py
+++ tinymce/urls.py
@@ -1,17 +1,18 @@
 # Copyright (c) 2008 Joost Cassee
 # Licensed under the terms of the MIT License (see LICENSE.txt)
+from tinymce.views import textareas_js, spell_check, flatpages_link_list, compressor, filebrowser, preview
 
 try:
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url
 except:
-    from django.conf.urls.defaults import url, patterns
+    from django.conf.urls.defaults import url
 
-urlpatterns = patterns('tinymce.views',
-    url(r'^js/textareas/(?P<name>.+)/$', 'textareas_js', name='tinymce-js'),
-    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', 'textareas_js', name='tinymce-js-lang'),
-    url(r'^spellchecker/$', 'spell_check'),
-    url(r'^flatpages_link_list/$', 'flatpages_link_list'),
-    url(r'^compressor/$', 'compressor', name='tinymce-compressor'),
-    url(r'^filebrowser/$', 'filebrowser', name='tinymce-filebrowser'),
-    url(r'^preview/(?P<name>.+)/$', 'preview', name='tinymce-preview'),
-)
+urlpatterns = [
+    url(r'^js/textareas/(?P<name>.+)/$', textareas_js, name='tinymce-js'),
+    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', textareas_js, name='tinymce-js-lang'),
+    url(r'^spellchecker/$', spell_check),
+    url(r'^flatpages_link_list/$', flatpages_link_list),
+    url(r'^compressor/$', compressor, name='tinymce-compressor'),
+    url(r'^filebrowser/$', filebrowser, name='tinymce-filebrowser'),
+    url(r'^preview/(?P<name>.+)/$', preview, name='tinymce-preview'),
+]
