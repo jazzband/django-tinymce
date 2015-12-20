78b418dbc8c478c09ab2c13c38167cb916bb2bbf
Joel Burton <joel@joelburton.com>
2015-12-19 22:59:16 -0500

    Updated urls.py to use modern conventions:
    - don't use string imports
    
    - don't use partterns()

--- tinymce/urls.py
+++ tinymce/urls.py
@@ -1,15 +1,14 @@
 # Copyright (c) 2008 Joost Cassee
 # Licensed under the terms of the MIT License (see LICENSE.txt)
+from django.conf.urls import url
+
 from tinymce import views
 
-try:
-    from django.conf.urls import url
-except:
-    from django.conf.urls.defaults import url
-
 urlpatterns = [
-    url(r'^js/textareas/(?P<name>.+)/$', views.textareas_js, name='tinymce-js'),
-    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', views.textareas_js, name='tinymce-js-lang'),
+    url(r'^js/textareas/(?P<name>.+)/$', views.textareas_js,
+        name='tinymce-js'),
+    url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', views.textareas_js,
+        name='tinymce-js-lang'),
     url(r'^spellchecker/$', views.spell_check),
     url(r'^flatpages_link_list/$', views.flatpages_link_list),
     url(r'^compressor/$', views.compressor, name='tinymce-compressor'),
