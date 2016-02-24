f2922191c3d9c2517e3fd5aefca874135022a889
vanadium23 <chernoffivan@gmail.com>
2016-02-24 20:57:13 +0300

    [Tests] add test for two views

--- tinymce/tests/test_views.py
+++ tinymce/tests/test_views.py
@@ -1,41 +1,43 @@
 # coding: utf-8
 
+from mock import patch
+
+from django.contrib.flatpages.models import FlatPage
 from django.test import TestCase
+
+from tinymce.views import render_to_image_list
 
 
 class TestViews(TestCase):
-    def test_textarea_js(self):
-        # TODO: implement test for textarea
-        pass
-
-    def test_spellchecker(self):
+    def test_spell_check(self):
         # TODO: implement test for spellchecker
         pass
 
-    def test_preview(self):
-        # TODO: implement test for preview
-        pass
-
     def test_flatpages_link_list(self):
-        # TODO: implement test
-        pass
+        FlatPage.objects.create(
+            url='/test/url',
+            title='Test Title',
+        )
+        response = self.client.get('/tinymce/flatpages_link_list/')
+        result_ok = 'var tinyMCELinkList = [["Test Title", "/test/url"]];'
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('application/x-javascript', response['Content-Type'])
+        self.assertEqual(result_ok, response.content)
 
     def test_compressor(self):
         # TODO: implement test
         pass
 
-    def test_render_to_link_list(self):
-        # TODO: implement test
-        pass
-
     def test_render_to_image_list(self):
-        # TODO: implement test
-        pass
+        response = render_to_image_list([('test', 'test.jpg')])
+        result_ok = 'var tinyMCEImageList = [["test", "test.jpg"]];'
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('application/x-javascript', response['Content-Type'])
+        self.assertEqual(result_ok, response.content)
 
-    def test_render_to_js_vardef(self):
-        # TODO: implement test
-        pass
-
-    def test_filebrowser(self):
-        # TODO: implement test
-        pass
+    @patch('tinymce.views.urlresolvers.reverse', return_value='/filebrowser')
+    def test_filebrowser(self, reverse_mock):
+        response = self.client.get('/tinymce/filebrowser/')
+        response_ok = 'function djangoFileBrowser(field_name, url, type, win) {\n    var url = "http://testserver/filebrowser?pop=2&type=" + type;\n\n    tinyMCE.activeEditor.windowManager.open(\n        {\n            \'file\': url,\n            \'width\': 820,\n            \'height\': 500,\n            \'resizable\': "yes",\n            \'scrollbars\': "yes",\n            \'inline\': "no",\n            \'close_previous\': "no"\n        },\n        {\n            \'window\': win,\n            \'input\': field_name,\n            \'editor_id\': tinyMCE.selectedInstance.editorId\n        }\n    );\n    return false;\n}\n'
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response_ok, response.content)
