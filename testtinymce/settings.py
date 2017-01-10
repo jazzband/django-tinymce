ad320c0234bd25bc15f8ead5a8dd4b2e969c9a9f
Vladyslav <vladyslav.krylasov@gmail.com>
2017-01-10 13:05:54 +0200

    Add compatibility for Django 1.10.5

--- testtinymce/settings.py
+++ testtinymce/settings.py
@@ -41,6 +41,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth'
+            ]
+        },
     },
 ]
 
